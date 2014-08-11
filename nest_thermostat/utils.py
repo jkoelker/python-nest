a62e7f9ed37ee7b8839620ac70e04be362d8142a
Jason Kölker <jason@koelker.net>
2014-08-11 00:42:38 +0000

    Use `decimal.Decimal` for conversions
    Helps to ensure precision

--- nest_thermostat/utils.py
+++ nest_thermostat/utils.py
@@ -1,12 +1,18 @@
 #-*- coding:utf-8 -*-
 
+import decimal
+
 CELSIUS = 'C'
 FAHRENHEIT = 'F'
+_THIRTYTWO = decimal.Decimal(32)
+_ONEPOINTEIGHT = decimal.Decimal(18) / decimal.Decimal(10)
 
 
 def f_to_c(temp):
-    return (temp - 32.0) / 1.8
+    temp = decimal.Decimal(temp)
+    return float((temp - _THIRTYTWO) / _ONEPOINTEIGHT)
 
 
 def c_to_f(temp):
-    return temp * 1.8 + 32.0
+    temp = decimal.Decimal(temp)
+    return float(temp * _ONEPOINTEIGHT + _THIRTYTWO)
