11747390cacfb0dd25d7675723c2c5aa4b2b33a5
Jason Kölker <jason@koelker.net>
2015-01-12 22:22:26 +0000

    Rename for pypi
    The python module is now named `nest` which pyth pypi name is
    `python-nest`.

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 
 from setuptools import setup
 
-setup(name='nest-thermostat',
-      version='1.2',
+setup(name='python-nest',
+      version='2.0',
       description='Python API and command line tool for talking to the '
                   'Nest™ Thermostat',
-      author='Scott Baker',
-      author_email='smbaker@gmail.com',
-      maintainer='Filippo Valsorda',
-      maintainer_email='hi@filippo.io',
-      url='https://github.com/FiloSottile/nest_thermostat/',
-      packages=['nest_thermostat'],
+      long_description=open('README.rst').read(),
+      keywords='nest thermostat',
+      author='Jason Kölker',
+      author_email='jason@koelker.net',
+      url='https://github.com/jkoelker/python-nest/',
+      packages=['nest'],
       install_requires=['requests'],
       entry_points={
           'console_scripts': ['nest=nest_thermostat.command_line:main'],
