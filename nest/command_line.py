c3a1e55dd11250ddf4366cb21b91d0d5abf5987f
Troy Fontaine <tfontaine@troyfontaine.com>
2017-07-08 16:59:29 -0500

    modified:   nest/command_line.py

--- nest/command_line.py
+++ nest/command_line.py
@@ -287,7 +287,7 @@ def main():
             if device.has_hot_water_control:
                 print('Hot Water Temp        : %s' % device.fan)
             print('Temp                  : %0.1f%s' %
-                 (device.temperature, device.temperature_scale))
+                  (device.temperature, device.temperature_scale))
             print('Humidity              : %0.1f%%' % device.humidity)
             if isinstance(device.target, tuple):
                 print('Target                 : %0.1f-%0.1f%s' % (
@@ -296,7 +296,7 @@ def main():
                     device.temperature_scale))
             else:
                 print('Target                : %0.1f%s' %
-                     (display_temp(device.target), device.temperature_scale))
+                      (display_temp(device.target), device.temperature_scale))
             print('Away Heat             : %0.1fC' % device.eco_temperature[0])
             print('Away Cool             : %0.1fC' % device.eco_temperature[1])
             print('Has Leaf              : %s' % device.has_leaf)
