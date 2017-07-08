a0c59f50b37e68bc3b607642b19ee568163e38b5
Troy Fontaine <tfontaine@troyfontaine.com>
2017-07-08 16:54:24 -0500

    modified:   nest/command_line.py

--- nest/command_line.py
+++ nest/command_line.py
@@ -296,7 +296,7 @@ def main():
                     device.temperature_scale))
             else:
                 print('Target                : %0.1f%s' %
-                    (display_temp(device.target), device.temperature_scale))
+                        (display_temp(device.target), device.temperature_scale))
             print('Away Heat             : %0.1fC' % device.eco_temperature[0])
             print('Away Cool             : %0.1fC' % device.eco_temperature[1])
             print('Has Leaf              : %s' % device.has_leaf)
