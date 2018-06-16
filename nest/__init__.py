017e6de75854bae67a6cfecfb90d154bfb27ad7d
Jason Hu <awaregit@gmail.com>
2018-06-16 00:41:16 -0700

    Add logging

--- nest/__init__.py
+++ nest/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding:utf-8 -*-
+import logging
 
 from .nest import Nest
 
 from .utils import CELSIUS
 from .utils import FAHRENHEIT
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = ['CELSIUS', 'FAHRENHEIT', 'Nest']
