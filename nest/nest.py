f3b0d0b0b30c101edd58102cd6c556beeaf67eb6
Jason Hu <awaregit@gmail.com>
2018-06-21 23:12:00 -0700

    Guard  None access

--- nest/nest.py
+++ nest/nest.py
@@ -269,8 +269,11 @@ class Device(NestBase):
 
     @property
     def structure(self):
-        return Structure(self._device['structure_id'],
-                         self._nest_api)
+        if 'structure_id' in self._device:
+            return Structure(self._device['structure_id'],
+                             self._nest_api)
+        else:
+            return None
 
     @property
     def where(self):
@@ -299,7 +302,7 @@ class Device(NestBase):
 
     @property
     def description(self):
-        return self._device['name_long']
+        return self._device.get('name_long')
 
     @property
     def is_thermostat(self):
@@ -321,7 +324,7 @@ class Thermostat(Device):
 
     @property
     def _device(self):
-        return self._devices[THERMOSTATS][self._serial]
+        return self._devices.get(THERMOSTATS, {}).get(self._serial, {})
 
     @property
     def _shared(self):
@@ -335,7 +338,7 @@ class Thermostat(Device):
 
     @property
     def software_version(self):
-        return self._device['software_version']
+        return self._device.get('software_version')
 
     @property
     def fan(self):
@@ -509,7 +512,7 @@ class Thermostat(Device):
 
     @property
     def temperature_scale(self):
-        return self._device['temperature_scale']
+        return self._device.get('temperature_scale')
 
     @property
     def is_locked(self):
@@ -552,11 +555,11 @@ class Thermostat(Device):
     @property
     def target(self):
         if self.mode == 'heat-cool':
-            low = self._device[self._temp_key('target_temperature_low')]
-            high = self._device[self._temp_key('target_temperature_high')]
+            low = self._device.get(self._temp_key('target_temperature_low'))
+            high = self._device.get(self._temp_key('target_temperature_high'))
             return LowHighTuple(low, high)
 
-        return self._device[self._temp_key('target_temperature')]
+        return self._device.get(self._temp_key('target_temperature'))
 
     @target.setter
     def target(self, value):
@@ -677,7 +680,7 @@ class SmokeCoAlarm(Device):
 
     @property
     def _device(self):
-        return self._devices[SMOKE_CO_ALARMS][self._serial]
+        return self._devices.get(SMOKE_CO_ALARMS, {}).get(self._serial, {})
 
     @property
     def auto_away(self):
@@ -1104,7 +1107,7 @@ class Camera(Device):
 
     @property
     def _device(self):
-        return self._devices[CAMERAS][self._serial]
+        return self._devices.get(CAMERAS, {}).get(self._serial, {})
 
     @property
     def ongoing_event(self):
@@ -1348,7 +1351,8 @@ class Camera(Device):
 class Structure(NestBase):
     @property
     def _structure(self):
-        return self._nest_api._status[STRUCTURES][self._serial]
+        return self._nest_api._status.get(
+            STRUCTURES, {}).get(self._serial, {})
 
     def __repr__(self):
         return str(self._structure)
@@ -1889,15 +1893,15 @@ class Nest(object):
 
     @property
     def _metadata(self):
-        return self._status[METADATA]
+        return self._status.get(METADATA, {})
 
     @property
     def client_version(self):
-        return self._metadata['client_version']
+        return self._metadata.get('client_version')
 
     @property
     def _devices(self):
-        return self._status[DEVICES]
+        return self._status.get(DEVICES, {})
 
     @property
     def devices(self):
@@ -1929,7 +1933,7 @@ class Nest(object):
     @property
     def structures(self):
         return [Structure(stid, self)
-                for stid in self._status[STRUCTURES]]
+                for stid in self._status.get(STRUCTURES, [])]
 
     @property
     def urls(self):
